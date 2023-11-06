3c4caf267fd32eb1035a3cbfa05eb8005b576b40
Ben Ross <mrbenross@gmail.com>
2023-11-06 12:40:18 -0600

    new files added

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './WeatherApp.css'
+const WeatherApp = () =>{
+    return(
+        <div>
+        <h1>Test</h1>
+        </div>
+    )
+}
+export default WeatherApp
